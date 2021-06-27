54a082721b141baf8d5b1e1d7ff26cec553bb347
nmisustin <nmisustin@gmail.com>
2021-06-27 09:43:21 -0400

    added user model

---
+++ models/Users.js
@@ -0,0 +1,33 @@
+const{Schema, model} = require('mongoose');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true 
+    },
+    email: {
+        type: String,
+        validate: {
+            validator: function(v){
+                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+            }
+        },
+        trim: true
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+})
+const User = model('User', userSchema);
+module.exports = User;
