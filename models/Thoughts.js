af1a6f59b7ed02a00bb0c17266753d293eaed1a6
nmisustin <nmisustin@gmail.com>
2021-06-27 10:05:42 -0400

    Added the thought model

---
+++ models/Thoughts.js
@@ -0,0 +1,50 @@
+const{Schema, model, Types} = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt : {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true 
+        },
+        reactions: [
+            reactionSchema
+        ]
+    }
+)
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: ()=> new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        username: {
+            type: String,
+            required: true 
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    }
+)
+thoughtSchema.virtual('reactionCount').get(function(){
+    return this.reactions.length;
+})
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
--- models/Users.js
+++ models/Users.js
@@ -1,33 +1,44 @@
-const{Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const userSchema = new Schema({
-    username: {
-        type: String,
-        trim: true,
-        unique: true,
-        required: true 
-    },
-    email: {
-        type: String,
-        validate: {
-            validator: function(v){
-                return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
-            }
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            trim: true,
+            unique: true,
+            required: true
         },
-        trim: true
+        email: {
+            type: String,
+            validate: {
+                validator: function (v) {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+                }
+            },
+            trim: true
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
     },
-    thoughts: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }
-    ],
-    friends: [
-        {
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }
-    ]
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+)
+userSchema.virtual('friendCount').get(function () {
+    return friends.length
 })
 const User = model('User', userSchema);
 module.exports = User;
