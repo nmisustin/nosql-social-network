9e188d00e128c329ac7425e86bd048b87404a96d
nmisustin <nmisustin@gmail.com>
2021-06-27 16:46:54 -0400

    set up server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nosql-social-network', {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true   
+});
+mongoose.set('dbug', true);
+
+app.use(require('./routes'));
+app.listen(PORT, () => console.log(`you are now connected on ${PORT}`));
